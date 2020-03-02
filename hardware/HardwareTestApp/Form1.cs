18d213749088e245eafa988e00ae932ccea5508c
Joshua-Jurgensmeier <joshjurg777@gmail.com>
2020-03-01 22:46:11 -0600

    Initial commit for Hardware

---
+++ hardware/HardwareTestApp/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NCR_Printer;
+
+
+namespace HardwareTestApp
+{
+    public partial class Form1 : Form
+    {
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_OnClick(object sender, EventArgs e)
+        {
+            Image logo = Image.FromFile("Z:\\SecretCellar\\hardware\\logo.bmp");
+            Receipt r = new Receipt(logo, "Secret Cellar", new double[] { 0.99, 1.35, 4415.34 });
+            r.print();
+        }
+    }
+}
