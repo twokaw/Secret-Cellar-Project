14cb45315631c93579481b807cc801f03c3f588f
SecretCellarFront <60951916+SecretCellarFront@users.noreply.github.com>
2020-03-12 20:26:52 -0500

    created DLL
    hopefully this works team

---
+++ shared/Models/Models/Supplier.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace Models
+{
+    public class Supplier : ISerializable
+    {
+        public string Name;
+        public string PhoneNumber;
+
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("Name", Name);
+            info.AddValue("PhoneNumber", PhoneNumber);
+        }
+    }
+}
