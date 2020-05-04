8e2a3744af76ff9ccc12e62697759cbf6a1a21f6
Joshua-Jurgensmeier <joshjurg777@gmail.com>
2020-05-04 15:13:55 -0500

    Some modifications to print routine.

--- hardware/HardwareTestApp/Form1.cs
+++ hardware/HardwareTestApp/Form1.cs
@@ -25,12 +25,22 @@ namespace HardwareTestApp
         {
 
             List<Item> items = new List<Item>();
-            items.Add(new Item("Wine", 911, 911, 3, 1, 999999.99M, true, "WINE", 1));
-            Transaction trans = new Transaction(1, 1, DateTime.Now, "Shueyville", items, 0.0M, 99.99M, 0.0M, 99.99M, false, "Card", "1234");
+            items.Add(new Item("Wine", 911, 911, 3, 1, 99.99M, true, "WINE", 1));
+            Transaction trans = new Transaction(2286, 1, DateTime.Now, "Shueyville, IA", items, 0.0M, 99.99M, 0.13M, 99.99M+0.13M, false, "Card", "1234");
 
-            Image logo = Image.FromFile("Z:\\SecretCellar\\hardware\\logo.bmp");
-            Receipt r = new Receipt(trans, "HEADER", "FOOTER", logo);
+            Image logo = Image.FromFile("Z:\\SecretCellar\\hardware\\SecretCellar.jpg");
+            Receipt r = new Receipt(trans, "The Secret Cellar\n(319) 841-2172\n1205 Curtis Bridge Rd NE", "Thank you!\nCheck our website for events and classes\nwww.SecretCellarwines.com", logo);
             r.print();
+
+            List<Item> items2 = new List<Item>();
+            items.Add(new Item("Wine", 911, 911, 3, 1, 9.99M, true, "WINE", 1));
+            items.Add(new Item("Fancy Wine", 911, 911, 3, 1, 99.99M, true, "WINE", 1));
+            items.Add(new Item("Fanciest Wine", 911, 911, 3, 1, 999.99M, true, "WINE", 1));
+            Transaction trans2 = new Transaction(1234, 42, DateTime.Now, "The Moon", items2, 0.0M, 1109.97M, 20M, 1129.97M, false, "CHK", "1234");
+
+            Image logo2 = Image.FromFile("Z:\\SecretCellar\\hardware\\logo.bmp");
+            Receipt r2 = new Receipt(trans2, "The Secret Cellar\n(319) 841-2172\n1205 Curtis Bridge Rd NE", "Thank you!\nCheck our website for events and classes\nwww.SecretCellarwines.com", logo2);
+            r2.print();
 
         }
     }
--- hardware/NCR_Printer/Receipt.cs
+++ hardware/NCR_Printer/Receipt.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Printing;
+using System.Linq;
 using Shared;
 
 
@@ -74,84 +75,101 @@ namespace NCR_Printer
             // Initialize printing stuff
             Graphics g = e.Graphics;
 
-            Font font = new Font("Arial", 12);
+            Font font = new Font("Arial", 10);
+            Font barcodeFont = new Font("Free 3 of 9", 36);
+            float lSpacing = 5F;
             Brush brush = Brushes.Black;
-
+            Pen pen = new Pen(brush);
+            
             StringFormat leftAlign = new StringFormat() { Alignment = StringAlignment.Near };
-            StringFormat rightAlign = new StringFormat() { Alignment = StringAlignment.Far };
             StringFormat centerAlign = new StringFormat() { Alignment = StringAlignment.Center };
+            StringFormat rightAlign = new StringFormat() { Alignment = StringAlignment.Far };
 
             RectangleF cursor = new RectangleF(e.PageBounds.X, e.PageBounds.Y, e.PageBounds.Width, font.Height);
 
             // Print Logo
             g.DrawImage(logo, cursor.Width/2-logo.Width/2, cursor.Top);
-            cursor.Y += logo.Height;
+            cursor.Y += logo.Height + lSpacing;
 
             // Print InvoiceID Barcode
-            g.DrawString(InvoiceID.ToString(), font, brush, cursor, leftAlign);
-            cursor.Y += font.Height;
+            g.DrawString("*" + InvoiceID.ToString() + "*", barcodeFont, brush, cursor, centerAlign);
+            cursor.Y += barcodeFont.Height + lSpacing;
 
             // Print Header
-            g.DrawString(header, font, brush, cursor, centerAlign);
-            cursor.Y += font.Height; //TODO: Will nee to handle multiline headers well
+            string[] headerLines = header.Split('\n');
+            foreach (string line in headerLines) {
+                g.DrawString(line, font, brush, cursor, centerAlign);
+                cursor.Y += font.Height + lSpacing;
+            }
 
             // Print InvoiceID
             g.DrawString("Invoice #: " + InvoiceID.ToString(), font, brush, cursor, leftAlign);
-            
+            cursor.Y += font.Height + lSpacing;
+
+            // Print RegisterID
+            g.DrawString("Register ID: " + RegisterID.ToString(), font, brush, cursor, leftAlign);
 
             // Print Location
-            g.DrawString("Location" + Location, font, brush, cursor, rightAlign);
-            cursor.Y += font.Height;
+            g.DrawString("Location: " + Location, font, brush, cursor, rightAlign);
+            cursor.Y += font.Height + lSpacing;
 
             // Date and time
             g.DrawString("Date and time: " + TransactionDateTime.ToString(), font, brush, cursor, leftAlign);
-            cursor.Y += font.Height;
+            cursor.Y += font.Height + lSpacing;
+
+            // Print Horizontal line
+            cursor.Y += (font.Height + lSpacing) / 2;
+            g.DrawLine(pen, cursor.X, cursor.Y, cursor.X + cursor.Width, cursor.Y);
+            cursor.Y += (font.Height + lSpacing) / 2;
 
             // Print Items
-            g.DrawString("---------------------------------", font, brush, cursor, leftAlign);
-            cursor.Y += font.Height;
-
             foreach (Item item in Items)
             {
                 g.DrawString(item.NumSold + " " + item.Name, font, brush, cursor, leftAlign);
                 g.DrawString("$ " + item.Price.ToString(), font, brush, cursor, rightAlign);
-                cursor.Y += font.Height;
+                cursor.Y += font.Height + lSpacing;
             }
 
-            g.DrawString("---------------------------------", font, brush, cursor, leftAlign);
-            cursor.Y += font.Height;
+            // Print Horizontal line
+            cursor.Y += (font.Height + lSpacing) / 2;
+            g.DrawLine(pen, cursor.X, cursor.Y, cursor.X + cursor.Width, cursor.Y);
+            cursor.Y += (font.Height + lSpacing) / 2;
 
             // Print subtotal
             g.DrawString("SubTotal:", font, brush, cursor, leftAlign);
             g.DrawString("$ " + Subtotal.ToString(), font, brush, cursor, rightAlign);
-            cursor.Y += font.Height;
+            cursor.Y += font.Height + lSpacing;
 
             // Print tax
             g.DrawString("Tax:", font, brush, cursor, leftAlign);
             g.DrawString("$ " + Tax.ToString(), font, brush, cursor, rightAlign);
-            cursor.Y += font.Height;
+            cursor.Y += font.Height + lSpacing;
 
             // Print total
             g.DrawString("TOTAL:", font, brush, cursor, leftAlign);
             g.DrawString("$ " + Total.ToString(), font, brush, cursor, rightAlign);
-            cursor.Y += font.Height;
+            cursor.Y += font.Height + lSpacing;
 
-            g.DrawString("---------------------------------", font, brush, cursor, leftAlign);
-            cursor.Y += font.Height;
+            // Print Horizontal line
+            cursor.Y += (font.Height + lSpacing) / 2;
+            g.DrawLine(pen, cursor.X, cursor.Y, cursor.X + cursor.Width, cursor.Y);
+            cursor.Y += (font.Height + lSpacing) / 2;
 
             // Print # of items
             g.DrawString("# of Items: " + Items.Count.ToString(), font, brush, cursor, leftAlign);
-            cursor.Y += font.Height;
+            cursor.Y += font.Height + lSpacing;
 
             // Print Payment Method
-            // Print # of items
             g.DrawString("Payment Method: " + PayMethod + ": " + PayNum, font, brush, cursor, leftAlign);
-            cursor.Y += font.Height;
+            cursor.Y += font.Height + lSpacing;
 
             //Print footer
-            // Print # of items
-            g.DrawString(footer, font, brush, cursor, centerAlign);
-            cursor.Y += font.Height; //TODO: Will nee to handle multiline footers well
+            string[] footerLines = footer.Split('\n');
+            foreach (string line in footerLines)
+            {
+                g.DrawString(line, font, brush, cursor, centerAlign);
+                cursor.Y += font.Height + lSpacing;
+            }
         }
     }
 }
