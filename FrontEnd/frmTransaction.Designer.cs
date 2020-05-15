5ddf1b06ac819ecd0626a9ad39a0983c7bad3b8e
Dave <35737794+twokaw@users.noreply.github.com>
2020-05-14 19:18:05 -0500

    update trans form

--- FrontEnd/frmTransaction.Designer.cs
+++ FrontEnd/frmTransaction.Designer.cs
@@ -47,15 +47,16 @@
             this.btnVoidTrx = new System.Windows.Forms.Button();
             this.btnDeleteItem = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.txt_transBTLDPT = new System.Windows.Forms.TextBox();
+            this.txt_transSubTotal = new System.Windows.Forms.TextBox();
+            this.LB_Discount = new System.Windows.Forms.Label();
             this.LB_TOTAL = new System.Windows.Forms.Label();
             this.LB_BTLDPT = new System.Windows.Forms.Label();
             this.LB_Tax = new System.Windows.Forms.Label();
             this.LB_SubTotal = new System.Windows.Forms.Label();
             this.txtBarcode = new System.Windows.Forms.TextBox();
             this.btnTender = new ePOSOne.btnProduct.Button_WOC();
-            this.LB_Discount = new System.Windows.Forms.Label();
-            this.txt_transSubTotal = new System.Windows.Forms.TextBox();
-            this.txt_transBTLDPT = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
@@ -81,13 +82,13 @@
             this.TAX,
             this.BOTTLE_DEPOSIT,
             this.TOTAL});
-            this.dataGridView1.Location = new System.Drawing.Point(274, 155);
+            this.dataGridView1.Location = new System.Drawing.Point(445, 152);
             this.dataGridView1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersVisible = false;
             this.dataGridView1.RowHeadersWidth = 102;
             this.dataGridView1.RowTemplate.Height = 40;
-            this.dataGridView1.Size = new System.Drawing.Size(1757, 458);
+            this.dataGridView1.Size = new System.Drawing.Size(1354, 458);
             this.dataGridView1.TabIndex = 2;
             this.dataGridView1.TabStop = false;
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
@@ -263,12 +264,39 @@
             this.panel1.Controls.Add(this.LB_Tax);
             this.panel1.Controls.Add(this.LB_SubTotal);
             this.panel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.panel1.Location = new System.Drawing.Point(1693, 710);
+            this.panel1.Location = new System.Drawing.Point(1699, 749);
             this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(455, 286);
             this.panel1.TabIndex = 13;
             this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // txt_transBTLDPT
+            // 
+            this.txt_transBTLDPT.Location = new System.Drawing.Point(277, 170);
+            this.txt_transBTLDPT.Name = "txt_transBTLDPT";
+            this.txt_transBTLDPT.ReadOnly = true;
+            this.txt_transBTLDPT.Size = new System.Drawing.Size(151, 45);
+            this.txt_transBTLDPT.TabIndex = 6;
+            // 
+            // txt_transSubTotal
+            // 
+            this.txt_transSubTotal.Location = new System.Drawing.Point(277, 13);
+            this.txt_transSubTotal.Name = "txt_transSubTotal";
+            this.txt_transSubTotal.ReadOnly = true;
+            this.txt_transSubTotal.Size = new System.Drawing.Size(151, 45);
+            this.txt_transSubTotal.TabIndex = 5;
+            this.txt_transSubTotal.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // LB_Discount
+            // 
+            this.LB_Discount.AutoSize = true;
+            this.LB_Discount.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.1F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LB_Discount.Location = new System.Drawing.Point(6, 69);
+            this.LB_Discount.Name = "LB_Discount";
+            this.LB_Discount.Size = new System.Drawing.Size(198, 32);
+            this.LB_Discount.TabIndex = 4;
+            this.LB_Discount.Text = "DISC. TOTAL";
             // 
             // LB_TOTAL
             // 
@@ -331,13 +359,13 @@
             this.btnTender.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
             this.btnTender.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnTender.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnTender.Location = new System.Drawing.Point(1188, 767);
+            this.btnTender.Location = new System.Drawing.Point(1225, 801);
             this.btnTender.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnTender.Name = "btnTender";
             this.btnTender.OnHoverBorderColor = System.Drawing.Color.Transparent;
             this.btnTender.OnHoverButtonColor = System.Drawing.Color.FromArgb(((int)(((byte)(127)))), ((int)(((byte)(24)))), ((int)(((byte)(24)))));
             this.btnTender.OnHoverTextColor = System.Drawing.Color.Black;
-            this.btnTender.Size = new System.Drawing.Size(440, 258);
+            this.btnTender.Size = new System.Drawing.Size(383, 190);
             this.btnTender.TabIndex = 14;
             this.btnTender.TabStop = false;
             this.btnTender.Text = "TENDER";
@@ -345,32 +373,14 @@
             this.btnTender.UseVisualStyleBackColor = false;
             this.btnTender.Click += new System.EventHandler(this.btnTender_Click);
             // 
-            // LB_Discount
+            // label1
             // 
-            this.LB_Discount.AutoSize = true;
-            this.LB_Discount.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.1F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.LB_Discount.Location = new System.Drawing.Point(6, 69);
-            this.LB_Discount.Name = "LB_Discount";
-            this.LB_Discount.Size = new System.Drawing.Size(198, 32);
-            this.LB_Discount.TabIndex = 4;
-            this.LB_Discount.Text = "DISC. TOTAL";
-            // 
-            // txt_transSubTotal
-            // 
-            this.txt_transSubTotal.Location = new System.Drawing.Point(277, 13);
-            this.txt_transSubTotal.Name = "txt_transSubTotal";
-            this.txt_transSubTotal.ReadOnly = true;
-            this.txt_transSubTotal.Size = new System.Drawing.Size(151, 45);
-            this.txt_transSubTotal.TabIndex = 5;
-            this.txt_transSubTotal.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
-            // 
-            // txt_transBTLDPT
-            // 
-            this.txt_transBTLDPT.Location = new System.Drawing.Point(277, 170);
-            this.txt_transBTLDPT.Name = "txt_transBTLDPT";
-            this.txt_transBTLDPT.ReadOnly = true;
-            this.txt_transBTLDPT.Size = new System.Drawing.Size(151, 45);
-            this.txt_transBTLDPT.TabIndex = 6;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(764, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 32);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "label1";
             // 
             // frmTransaction
             // 
@@ -379,6 +389,7 @@
             this.AutoSize = true;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(63)))), ((int)(((byte)(64)))));
             this.ClientSize = new System.Drawing.Size(2296, 1245);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.txtBarcode);
             this.Controls.Add(this.btnTender);
             this.Controls.Add(this.panel1);
@@ -433,5 +444,6 @@
         private System.Windows.Forms.Label LB_Discount;
         private System.Windows.Forms.TextBox txt_transSubTotal;
         private System.Windows.Forms.TextBox txt_transBTLDPT;
+        private System.Windows.Forms.Label label1;
     }
 }
