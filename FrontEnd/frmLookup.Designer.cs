cb87b166c0201a5df54e86850cb8f433d07efb2a
ggrabau <37936446+ggrabau@users.noreply.github.com>
2020-06-05 10:26:35 -0500

    fixed form

--- FrontEnd/frmLookup.Designer.cs
+++ FrontEnd/frmLookup.Designer.cs
@@ -28,77 +28,70 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtlookup = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.Descrtion = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.LookupView = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.Id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Description = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Class = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Barcode = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Qty = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Price = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.LookupView)).BeginInit();
             this.SuspendLayout();
             // 
-            // textBox1
+            // txtlookup
             // 
-            this.textBox1.Location = new System.Drawing.Point(348, 54);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(240, 22);
-            this.textBox1.TabIndex = 0;
+            this.txtlookup.Location = new System.Drawing.Point(192, 21);
+            this.txtlookup.Margin = new System.Windows.Forms.Padding(2);
+            this.txtlookup.Name = "txtlookup";
+            this.txtlookup.Size = new System.Drawing.Size(181, 20);
+            this.txtlookup.TabIndex = 0;
+            this.txtlookup.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(175, 57);
+            this.label1.Location = new System.Drawing.Point(20, 21);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(168, 16);
             this.label1.TabIndex = 1;
             this.label1.Text = "Description or Barcode";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
-            // dataGridView1
+            // LookupView
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.Descrtion,
+            this.LookupView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.LookupView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.LookupView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Id,
+            this.Description,
+            this.Class,
             this.Barcode,
             this.Qty,
             this.Price});
-            this.dataGridView1.Location = new System.Drawing.Point(178, 139);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersVisible = false;
-            this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(444, 150);
-            this.dataGridView1.TabIndex = 2;
-            // 
-            // Descrtion
-            // 
-            this.Descrtion.HeaderText = "Name";
-            this.Descrtion.Name = "Descrtion";
-            // 
-            // Barcode
-            // 
-            this.Barcode.HeaderText = "Barcode";
-            this.Barcode.Name = "Barcode";
-            // 
-            // Qty
-            // 
-            this.Qty.HeaderText = "Qty";
-            this.Qty.Name = "Qty";
-            // 
-            // Price
-            // 
-            this.Price.HeaderText = "Price";
-            this.Price.Name = "Price";
+            this.LookupView.Location = new System.Drawing.Point(23, 54);
+            this.LookupView.Margin = new System.Windows.Forms.Padding(2);
+            this.LookupView.Name = "LookupView";
+            this.LookupView.RowHeadersVisible = false;
+            this.LookupView.RowTemplate.Height = 24;
+            this.LookupView.Size = new System.Drawing.Size(557, 257);
+            this.LookupView.TabIndex = 2;
             // 
             // button1
             // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(547, 310);
+            this.button1.Location = new System.Drawing.Point(515, 326);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(74, 30);
             this.button1.TabIndex = 3;
             this.button1.Text = "Cancel";
             this.button1.UseVisualStyleBackColor = true;
@@ -106,28 +99,78 @@
             // 
             // button2
             // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(178, 310);
+            this.button2.Location = new System.Drawing.Point(434, 326);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(97, 23);
+            this.button2.Size = new System.Drawing.Size(73, 30);
             this.button2.TabIndex = 4;
-            this.button2.Text = "Add Item";
+            this.button2.Text = "Add";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // Id
+            // 
+            this.Id.DataPropertyName = "Id";
+            this.Id.HeaderText = "ItemId";
+            this.Id.Name = "Id";
+            this.Id.Visible = false;
+            // 
+            // Description
+            // 
+            this.Description.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.Description.DataPropertyName = "Name";
+            this.Description.HeaderText = "Name";
+            this.Description.Name = "Description";
+            this.Description.Width = 60;
+            // 
+            // Class
+            // 
+            this.Class.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.Class.DataPropertyName = "ItemType";
+            this.Class.HeaderText = "Class";
+            this.Class.Name = "Class";
+            this.Class.Width = 57;
+            // 
+            // Barcode
+            // 
+            this.Barcode.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.Barcode.DataPropertyName = "Barcode";
+            this.Barcode.HeaderText = "Barcode";
+            this.Barcode.Name = "Barcode";
+            this.Barcode.Width = 72;
+            // 
+            // Qty
+            // 
+            this.Qty.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.Qty.DataPropertyName = "Qty";
+            this.Qty.HeaderText = "Qty";
+            this.Qty.Name = "Qty";
+            this.Qty.Width = 48;
+            // 
+            // Price
+            // 
+            this.Price.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.Price.DataPropertyName = "Price";
+            this.Price.HeaderText = "Price";
+            this.Price.Name = "Price";
+            this.Price.Width = 56;
+            // 
             // frmLookup
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(600, 366);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.LookupView);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtlookup);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "frmLookup";
             this.Text = "frmLookup";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.LookupView)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -135,14 +178,16 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtlookup;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Descrtion;
+        private System.Windows.Forms.DataGridView LookupView;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Description;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Class;
         private System.Windows.Forms.DataGridViewTextBoxColumn Barcode;
         private System.Windows.Forms.DataGridViewTextBoxColumn Qty;
         private System.Windows.Forms.DataGridViewTextBoxColumn Price;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
     }
 }
--- FrontEnd/frmLookup.cs
+++ FrontEnd/frmLookup.cs
@@ -15,11 +15,14 @@ namespace SecretCellar
     public partial class frmLookup : Form
     {
         private Transaction lookUp = null;
-        
-
-        public frmLookup()
+        private DataAccess dataAccess = new DataAccess(Properties.Settings.Default.URL);
+        private List<Inventory> inventory= null;
+        public frmLookup(Transaction transaction)
         {
+            lookUp = transaction;
             InitializeComponent();
+            inventory = dataAccess.GetInventory();
+            populate();
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -41,7 +44,7 @@ namespace SecretCellar
         private double addCharge()
         {
             // if (double.TryParse(txtPercentTotalSale.Text, out d))
-            lookUp.Subtotal += double.Parse(txtCharge.Text);
+           // lookUp.Subtotal += double.Parse(txtCharge.Text);
             return lookUp.Subtotal;
         }
         private void txtCharge_TextChanged(object sender, EventArgs e)
@@ -49,6 +52,33 @@ namespace SecretCellar
 
         }
 
+        private void populate()
+        {
+            LookupView.DataSource = inventory;
+            /*LookupView.Rows.Clear();
 
+            foreach (Inventory i in inventory.Where(x => (x.Qty > 0)))
+            {
+                int row = LookupView.Rows.Add();
+                using (var r = LookupView.Rows[row])
+                {
+                    r.Cells["ItemId"].Value = i.Id;
+                    r.Cells["Description"].Value = i.Name;
+                    r.Cells["CLASS"].Value = i.ItemType;
+                    r.Cells["Qty"].Value = i.Qty;
+                    r.Cells["Barcode"].Value = i.Barcode;
+                    r.Cells["Price"].Value = i.RetailPrice;
+                }
+            }*/
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            //BindingSource dv = ((DataTable)LookupView.DataSource).DefaultView;
+            //dv.RowFilter = $"Description like '%{txtlookup.Text}%'";
+           
+            LookupView.DataSource = inventory.Where(x=> x.Name.Contains(txtlookup.Text) || x.Barcode.Contains(txtlookup.Text)).
+                Select (x=> new { Name = x.Name, Id = x.Id, ItemType = x.ItemType, Qty = x.Qty, Barcode = x.Barcode, Price = x.Price }).ToList();
+        }
     }
 }
