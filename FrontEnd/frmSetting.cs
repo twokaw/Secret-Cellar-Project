c6a935935bf7a4b11163022f2ee09989c5c5fe0c
Bill Pitcher <wpitcher@mtmercy.edu>
2020-06-16 17:55:14 -0500

    Added settings form

---
+++ FrontEnd/frmSetting.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SecretCellar
+{
+    public partial class FrmSetting : Form
+    {
+        private readonly DataAccess dataAccess = new DataAccess();
+
+        public FrmSetting()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmSetting_Load(object sender, EventArgs e)
+        {
+            lstTypes.DataSource = dataAccess.GetInventoryType();
+        }
+    }
+}
