e105b997aa328ed88972bfab18b5748316c53936
Bill Pitcher <wjpitcher@gmail.com>
2020-07-28 18:12:29 -0500

    Added event

--- shared/Transaction/Event.cs
+++ shared/Transaction/Event.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Shared
 {
-    class Event
+    public class Event: BaseItem
     {
+        public Double PreOrder { get; set; }
+        public Double AtDoor { get; set; }
+        public DateTime Duration { get; set; }
+        public DateTime EventDate { get; set; }
     }
 }
