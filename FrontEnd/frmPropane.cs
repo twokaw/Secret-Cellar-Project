d2005b414f8e557c9f4b2fe0ac6980d14f400db4
Bill Pitcher <wjpitcher@gmail.com>
2020-08-29 17:34:57 -0500

    removed unused references

--- FrontEnd/frmPropane.cs
+++ FrontEnd/frmPropane.cs
@@ -1,15 +1,6 @@
 ﻿using System;
 using Shared;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using MySqlX.XDevAPI.Relational;
-
 namespace SecretCellar
 {
     public partial class frmPropane : Form
