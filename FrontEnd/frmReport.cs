efff4b1c33c225e91b7c5bd4824e40326f8d0dd2
Bill Pitcher <wjpitcher@gmail.com>
2020-09-09 23:00:36 -0500

    Add controls to report form, but no code yet ;)

--- FrontEnd/frmReport.cs
+++ FrontEnd/frmReport.cs
@@ -16,5 +16,15 @@ namespace SecretCellar
         {
             InitializeComponent();
         }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
