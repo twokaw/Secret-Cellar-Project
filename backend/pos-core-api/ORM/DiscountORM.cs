93f4e92f034014ec82259ab24795466119289780
Bill Pitcher <wjpitcher@gmail.com>
2020-12-03 20:05:37 -0600

    Added Discount

---
+++ backend/pos-core-api/ORM/DiscountORM.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApi.Helpers;
+
+namespace pos_core_api.ORM
+{
+    public class DiscountORM
+    {
+        readonly DbConn db = new DbConn();
+
+        private const string SQLGET = @"
+            SELECT receiptID, register, sold_datetime, customerID, empID, location, tax_exempt, discount, shipping
+            FROM transaction
+        ";
+    }
+}
