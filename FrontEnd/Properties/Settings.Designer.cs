0232d62948edd65a0b3d71701dbc7cd94995a9fd
ggrabau <37936446+ggrabau@users.noreply.github.com>
2020-12-15 19:38:40 -0600

    receipt font size

--- FrontEnd/Properties/Settings.Designer.cs
+++ FrontEnd/Properties/Settings.Designer.cs
@@ -88,7 +88,7 @@ namespace SecretCellar.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("11")]
+        [global::System.Configuration.DefaultSettingValueAttribute("10")]
         public float ReceiptFontSize {
             get {
                 return ((float)(this["ReceiptFontSize"]));
