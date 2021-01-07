d6bc9699f8f84141a9b21a2ad19246f19fb95f12
Bill Pitcher <wjpitcher@gmail.com>
2021-01-07 17:47:55 -0600

    adding customer withbalance

--- backend/pos-core-api/ORM/CustomerORM.cs
+++ backend/pos-core-api/ORM/CustomerORM.cs
@@ -12,16 +12,13 @@ namespace pos_core_api.ORM
 
         public List<Customer> Get()
         {
-
-            List<Customer> output = new List<Customer>();
-            Customer outputItem;
             db.OpenConnection();
 
             string sqlStatement = @"
               SELECT customerID, customer_discount, first_name, last_name,
                      business_name, email, isWholesale, 
                      addr1, addr2, city, state, zip, phone
-              FROM customer
+              FROM v_customer
             ";
 
             MySqlCommand cmd = new MySqlCommand(sqlStatement, db.Connection());
@@ -29,75 +26,100 @@ namespace pos_core_api.ORM
 
             try
             {
-                while (reader.Read())
-                {
-                    outputItem = new Customer
-                    {
-                        CustomerID = reader.IsDBNull("customerID") ? 0 : reader.GetUInt32("customerID"),
-                        CustomerDiscount = reader.IsDBNull("customer_discount") ? 0.0 : reader.GetDouble("customer_discount"),
-                        FirstName = reader.IsDBNull("first_name") ? "" : reader.GetString("first_name"),
-                        LastName = reader.IsDBNull("last_name") ? "" : reader.GetString("last_name"),
-                        BusinessName = reader.IsDBNull("business_name") ? "" : reader.GetString("business_name"),
-                        Email = reader.IsDBNull("email") ? "" : reader.GetString("email"),
-                        IsWholesale = !reader.IsDBNull("isWholesale") && reader.GetBoolean("isWholesale"),
-                        Address1 = reader.IsDBNull("addr1") ? "" : reader.GetString("addr1"),
-                        Address2 = reader.IsDBNull("addr2") ? "" : reader.GetString("addr2"),
-                        City = reader.IsDBNull("city") ? "" : reader.GetString("city"),
-                        State = reader.IsDBNull("state") ? "" : reader.GetString("state"),
-                        ZipCode = reader.IsDBNull("zip") ? "" : reader.GetString("zip"),
-                        PhoneNumber = reader.IsDBNull("phone") ? "" : reader.GetString("phone")
-                    };
-                    output.Add(outputItem);
-                }
+                return FetchCustomers(reader);
             }
             finally
             {
                 reader.Close();
                 db.CloseConnnection();
             }
-            return output;
+        }
+        public List<Customer> GetWithBalance()
+        {
+            db.OpenConnection();
+
+            string sqlStatement = @"
+              SELECT customerID, customer_discount, first_name, last_name,
+                     business_name, email, isWholesale, 
+                     addr1, addr2, city, state, zip, phone, balancedue, suspendedtransactions       
+              FROM v_customerwithbalance
+            ";
+
+            MySqlCommand cmd = new MySqlCommand(sqlStatement, db.Connection());
+            MySqlDataReader reader = cmd.ExecuteReader();
+
+            try
+            {
+                return FetchCustomers(reader, true);
+            }
+            finally
+            {
+                reader.Close();
+                db.CloseConnnection();
+            }
+        }
+        public Customer GetWithBalance(uint customerID)
+        {
+            db.OpenConnection();
+
+            string sqlStatement = @"
+              SELECT customerID, customer_discount, first_name, last_name,
+                     business_name, email, isWholesale, 
+                     addr1, addr2, city, state, zip, phone, balancedue, suspendedtransactions       
+              FROM v_customerwithbalance
+              WHERE customerID = @custID
+            ";
+
+            MySqlCommand cmd = new MySqlCommand(sqlStatement, db.Connection());
+            cmd.Parameters.Add(new MySqlParameter("custID", customerID));
+
+            MySqlDataReader reader = cmd.ExecuteReader();
+
+            try
+            {
+                List<Customer> output = FetchCustomers(reader, true);
+
+                if (output.Count > 0)
+                    return output[0];
+                else
+                    return new Customer();
+            }
+            finally
+            {
+                reader.Close();
+                db.CloseConnnection();
+            }
         }
 
         public Customer Get(uint customerID)
         {
-            Customer outputItem = new Customer();
             db.OpenConnection();
 
             string sqlStatement = @"
               SELECT customerID, customer_discount, first_name, last_name,
                      business_name, email, isWholesale, 
                      addr1, addr2, city, state, zip, phone
-              FROM customer 
+              FROM v_customer 
               WHERE customerID = @custID
             ";
 
             MySqlCommand cmd = new MySqlCommand(sqlStatement, db.Connection());
             cmd.Parameters.Add(new MySqlParameter("custID", customerID));
             MySqlDataReader reader = cmd.ExecuteReader();
-
             try
-            {
-                while (reader.Read())
-                {
-                    outputItem.CustomerID = reader.IsDBNull("customerID") ? 0 : reader.GetUInt32("customerID");
-                    outputItem.CustomerDiscount = reader.IsDBNull("customer_discount") ? 0.0 : reader.GetDouble("customer_discount");
-                    outputItem.FirstName = reader.IsDBNull("first_name") ? "" : reader.GetString("first_name");
-                    outputItem.LastName = reader.IsDBNull("last_name") ? "" : reader.GetString("last_name");
-                    outputItem.BusinessName = reader.IsDBNull("business_name") ? "" : reader.GetString("business_name");
-                    outputItem.Email = reader.IsDBNull("email") ? "" : reader.GetString("email");
-                    outputItem.IsWholesale = !reader.IsDBNull("isWholesale") && reader.GetBoolean("isWholesale");
-                    outputItem.Address1 = reader.IsDBNull("addr1") ? "" : reader.GetString("addr1");
-                    outputItem.Address2 = reader.IsDBNull("addr2") ? "" : reader.GetString("addr2");
-                    outputItem.City = reader.IsDBNull("city") ? "" : reader.GetString("city");
-                    outputItem.State = reader.IsDBNull("state") ? "" : reader.GetString("state");
-                    outputItem.ZipCode = reader.IsDBNull("zip") ? "" : reader.GetString("zip");
-                    outputItem.PhoneNumber = reader.IsDBNull("phone") ? "" : reader.GetString("phone");
-                }
-                return outputItem;
+            {           
+
+                List<Customer> output = FetchCustomers(reader);
+
+                if (output.Count > 0)
+                    return output[0];
+                else
+                    return new Customer();
             }
             finally
             {
                 reader.Close();
+                db.CloseConnnection();
             }
         }
 
@@ -194,6 +216,35 @@ namespace pos_core_api.ORM
             {
                 db.CloseConnnection();
             }
+        }
+
+        private List<Customer> FetchCustomers(MySqlDataReader reader, bool withBalance = false)
+        {
+            List<Customer> output = new List<Customer>();
+            Customer outputItem;
+            while (reader.Read())
+            {
+                outputItem = new Customer
+                {
+                    CustomerID = reader.IsDBNull("customerID") ? 0 : reader.GetUInt32("customerID"),
+                    CustomerDiscount = reader.IsDBNull("customer_discount") ? 0.0 : reader.GetDouble("customer_discount"),
+                    FirstName = reader.IsDBNull("first_name") ? "" : reader.GetString("first_name"),
+                    LastName = reader.IsDBNull("last_name") ? "" : reader.GetString("last_name"),
+                    BusinessName = reader.IsDBNull("business_name") ? "" : reader.GetString("business_name"),
+                    Email = reader.IsDBNull("email") ? "" : reader.GetString("email"),
+                    IsWholesale = !reader.IsDBNull("isWholesale") && reader.GetBoolean("isWholesale"),
+                    Address1 = reader.IsDBNull("addr1") ? "" : reader.GetString("addr1"),
+                    Address2 = reader.IsDBNull("addr2") ? "" : reader.GetString("addr2"),
+                    City = reader.IsDBNull("city") ? "" : reader.GetString("city"),
+                    State = reader.IsDBNull("state") ? "" : reader.GetString("state"),
+                    ZipCode = reader.IsDBNull("zip") ? "" : reader.GetString("zip"),
+                    PhoneNumber = reader.IsDBNull("phone") ? "" : reader.GetString("phone"),
+                    BalanceDue = reader.IsDBNull("balancedue") ? 0.0 : reader.GetDouble("balancedue"),
+                    SuspendedTransactions = reader.IsDBNull("suspendendtransactions") ? 0 : reader.GetUInt32("suspendendtransactions")
+                };
+                output.Add(outputItem);
+            }
+            return output;
         }
     }
 }
