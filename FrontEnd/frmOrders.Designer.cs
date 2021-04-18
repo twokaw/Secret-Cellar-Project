b199b33013c7180c0f6da826b2747bdda03e439d
ggrabau <37936446+ggrabau@users.noreply.github.com>
2021-04-18 11:15:18 -0500

    Added history
    Added history to orders panel

--- FrontEnd/frmOrders.Designer.cs
+++ FrontEnd/frmOrders.Designer.cs
@@ -61,12 +61,26 @@ namespace SecretCellar
             this.cust_name = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.prod_name = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.lstbox_customer = new System.Windows.Forms.ListBox();
+            this.transaction_dataGrid = new System.Windows.Forms.DataGridView();
+            this.lbl_end = new System.Windows.Forms.Label();
+            this.end_dateTime = new System.Windows.Forms.DateTimePicker();
+            this.lbl_start = new System.Windows.Forms.Label();
+            this.start_dateTime = new System.Windows.Forms.DateTimePicker();
             this.btn_close = new System.Windows.Forms.Button();
+            this.btn_print = new System.Windows.Forms.Button();
+            this.btn_reset = new System.Windows.Forms.Button();
+            this.txt_name = new System.Windows.Forms.TextBox();
+            this.lbl_name = new System.Windows.Forms.Label();
+            this.lbl_orders = new System.Windows.Forms.Label();
             this.supp_order.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.supp_dataGrid)).BeginInit();
             this.cust_request.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.request_dataGrid)).BeginInit();
             this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.transaction_dataGrid)).BeginInit();
             this.SuspendLayout();
             // 
             // supp_order
@@ -363,12 +377,83 @@ namespace SecretCellar
             // 
             this.tabControl1.Controls.Add(this.cust_request);
             this.tabControl1.Controls.Add(this.supp_order);
+            this.tabControl1.Controls.Add(this.tabPage1);
             this.tabControl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.tabControl1.Location = new System.Drawing.Point(12, 12);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
             this.tabControl1.Size = new System.Drawing.Size(1548, 496);
             this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.lbl_orders);
+            this.tabPage1.Controls.Add(this.lbl_name);
+            this.tabPage1.Controls.Add(this.txt_name);
+            this.tabPage1.Controls.Add(this.btn_reset);
+            this.tabPage1.Controls.Add(this.btn_print);
+            this.tabPage1.Controls.Add(this.lstbox_customer);
+            this.tabPage1.Controls.Add(this.transaction_dataGrid);
+            this.tabPage1.Controls.Add(this.lbl_end);
+            this.tabPage1.Controls.Add(this.end_dateTime);
+            this.tabPage1.Controls.Add(this.lbl_start);
+            this.tabPage1.Controls.Add(this.start_dateTime);
+            this.tabPage1.Location = new System.Drawing.Point(4, 29);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1540, 463);
+            this.tabPage1.TabIndex = 3;
+            this.tabPage1.Text = "History";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // lstbox_customer
+            // 
+            this.lstbox_customer.FormattingEnabled = true;
+            this.lstbox_customer.ItemHeight = 20;
+            this.lstbox_customer.Location = new System.Drawing.Point(70, 190);
+            this.lstbox_customer.Name = "lstbox_customer";
+            this.lstbox_customer.Size = new System.Drawing.Size(343, 164);
+            this.lstbox_customer.TabIndex = 5;
+            // 
+            // transaction_dataGrid
+            // 
+            this.transaction_dataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.transaction_dataGrid.Location = new System.Drawing.Point(959, 147);
+            this.transaction_dataGrid.Name = "transaction_dataGrid";
+            this.transaction_dataGrid.Size = new System.Drawing.Size(533, 270);
+            this.transaction_dataGrid.TabIndex = 4;
+            // 
+            // lbl_end
+            // 
+            this.lbl_end.AutoSize = true;
+            this.lbl_end.Location = new System.Drawing.Point(546, 194);
+            this.lbl_end.Name = "lbl_end";
+            this.lbl_end.Size = new System.Drawing.Size(41, 20);
+            this.lbl_end.TabIndex = 3;
+            this.lbl_end.Text = "End";
+            // 
+            // end_dateTime
+            // 
+            this.end_dateTime.Location = new System.Drawing.Point(601, 190);
+            this.end_dateTime.Name = "end_dateTime";
+            this.end_dateTime.Size = new System.Drawing.Size(284, 26);
+            this.end_dateTime.TabIndex = 2;
+            // 
+            // lbl_start
+            // 
+            this.lbl_start.AutoSize = true;
+            this.lbl_start.Location = new System.Drawing.Point(546, 151);
+            this.lbl_start.Name = "lbl_start";
+            this.lbl_start.Size = new System.Drawing.Size(49, 20);
+            this.lbl_start.TabIndex = 1;
+            this.lbl_start.Text = "Start";
+            // 
+            // start_dateTime
+            // 
+            this.start_dateTime.Location = new System.Drawing.Point(601, 147);
+            this.start_dateTime.Name = "start_dateTime";
+            this.start_dateTime.Size = new System.Drawing.Size(284, 26);
+            this.start_dateTime.TabIndex = 0;
             // 
             // btn_close
             // 
@@ -382,6 +467,49 @@ namespace SecretCellar
             this.btn_close.Text = "Close";
             this.btn_close.UseVisualStyleBackColor = true;
             this.btn_close.Click += new System.EventHandler(this.btn_close_Click);
+            // 
+            // btn_print
+            // 
+            this.btn_print.Location = new System.Drawing.Point(642, 306);
+            this.btn_print.Name = "btn_print";
+            this.btn_print.Size = new System.Drawing.Size(179, 48);
+            this.btn_print.TabIndex = 6;
+            this.btn_print.Text = "View/Print";
+            this.btn_print.UseVisualStyleBackColor = true;
+            // 
+            // btn_reset
+            // 
+            this.btn_reset.Location = new System.Drawing.Point(642, 369);
+            this.btn_reset.Name = "btn_reset";
+            this.btn_reset.Size = new System.Drawing.Size(179, 48);
+            this.btn_reset.TabIndex = 7;
+            this.btn_reset.Text = "Reset";
+            this.btn_reset.UseVisualStyleBackColor = true;
+            // 
+            // txt_name
+            // 
+            this.txt_name.Location = new System.Drawing.Point(209, 136);
+            this.txt_name.Name = "txt_name";
+            this.txt_name.Size = new System.Drawing.Size(204, 26);
+            this.txt_name.TabIndex = 8;
+            // 
+            // lbl_name
+            // 
+            this.lbl_name.AutoSize = true;
+            this.lbl_name.Location = new System.Drawing.Point(66, 139);
+            this.lbl_name.Name = "lbl_name";
+            this.lbl_name.Size = new System.Drawing.Size(137, 20);
+            this.lbl_name.TabIndex = 9;
+            this.lbl_name.Text = "Customer Name";
+            // 
+            // lbl_orders
+            // 
+            this.lbl_orders.AutoSize = true;
+            this.lbl_orders.Location = new System.Drawing.Point(955, 111);
+            this.lbl_orders.Name = "lbl_orders";
+            this.lbl_orders.Size = new System.Drawing.Size(104, 20);
+            this.lbl_orders.TabIndex = 10;
+            this.lbl_orders.Text = "Past Orders";
             // 
             // frmOrders
             // 
@@ -400,6 +528,9 @@ namespace SecretCellar
             this.cust_request.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.request_dataGrid)).EndInit();
             this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.transaction_dataGrid)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -440,5 +571,17 @@ namespace SecretCellar
         private System.Windows.Forms.DataGridViewTextBoxColumn minqty;
         private System.Windows.Forms.DataGridViewTextBoxColumn maxqty;
         private System.Windows.Forms.DataGridViewTextBoxColumn orderqty;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.ListBox lstbox_customer;
+        private System.Windows.Forms.DataGridView transaction_dataGrid;
+        private System.Windows.Forms.Label lbl_end;
+        private System.Windows.Forms.DateTimePicker end_dateTime;
+        private System.Windows.Forms.Label lbl_start;
+        private System.Windows.Forms.DateTimePicker start_dateTime;
+        private System.Windows.Forms.Button btn_reset;
+        private System.Windows.Forms.Button btn_print;
+        private System.Windows.Forms.Label lbl_name;
+        private System.Windows.Forms.TextBox txt_name;
+        private System.Windows.Forms.Label lbl_orders;
     }
 }
