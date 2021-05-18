375938c8d8b1fa9d2d361af97c06c7d82cd25163
ggrabau <37936446+ggrabau@users.noreply.github.com>
2021-05-18 18:32:16 -0500

    Added received order form
    Created a Order received form and added enter received  button to supply tab on orders form

---
+++ FrontEnd/frmReceivedOrders.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using SecretCellar;
+using Shared;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SecretCellar
+{
+    public partial class frmReceivedOrders : Form
+
+    {
+        private DataAccess dataAccess = new DataAccess(Properties.Settings.Default.URL);
+        private List<Supplier> suppliers = null;
+        private List<Inventory> inventory = null;
+        public frmReceivedOrders()
+        {
+            InitializeComponent();
+            inventory = dataAccess.GetInventory();
+            suppliers = dataAccess.GetSuppliers();
+            suppliers.Insert(0, new Supplier()
+            {
+                Name = "All",
+                SupplierID = 0
+
+            });
+
+            txt_received_qty.Focus();
+            received_dataGrid.Columns[4].DefaultCellStyle.Format = "C";
+            cbx_supplier.DataSource = suppliers;
+            cbx_supplier.DisplayMember = "Name";
+
+            received_dataGrid.DataSource = inventory.Where(x => x.OrderQty > 0).
+               Select(x => new {
+                   Name = x.Name,
+                   Id = x.Id,
+                   ItemType = x.ItemType,
+                   Qty = x.Qty,
+                   Barcode = x.Barcode,
+                   Price = x.SupplierPrice,
+                   minqty = x.InvMin,
+                   maxqty = x.InvMax,
+                   orderqty = x.OrderQty
+               }).
+               OrderBy(x => x.Name).
+               ToList();
+        }
+    }
+}
