65da3f4efaa1a755fb84647a943b89f95352c62d
Heem Othman <heem.othman@gmail.com>
2021-06-03 19:46:29 -0500

    Update frmCustom.Designer.cs

--- FrontEnd/frmCustom.Designer.cs
+++ FrontEnd/frmCustom.Designer.cs
@@ -147,7 +147,7 @@
 			this.MaximizeBox = false;
 			this.MinimizeBox = false;
 			this.Name = "frmCustom";
-			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
 			this.Text = "Add Custom Item";
 			this.ResumeLayout(false);
 			this.PerformLayout();
