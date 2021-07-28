5cbbabf78951d2ae974197a9a243b81f9c9d0aca
Heem Othman <heem.othman@gmail.com>
2021-07-27 19:42:22 -0500

    Added UI To The Panels
    -Added the UI from frmOrders into their respective panels. Added the panels into the tabs infrmOrdersPanels.

--- FrontEnd/Orders Panels/Panel_SupplierOrder.Designer.cs
+++ FrontEnd/Orders Panels/Panel_SupplierOrder.Designer.cs
@@ -24,10 +24,279 @@ namespace SecretCellar.Orders_Panels {
 		/// the contents of this method with the code editor.
 		/// </summary>
 		private void InitializeComponent() {
-			components = new System.ComponentModel.Container();
+			this.btn_recived = new System.Windows.Forms.Button();
+			this.btn_update = new System.Windows.Forms.Button();
+			this.txt_update_qty = new System.Windows.Forms.TextBox();
+			this.lbl_qty = new System.Windows.Forms.Label();
+			this.lbl_order_date = new System.Windows.Forms.Label();
+			this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+			this.btn_print_supp = new System.Windows.Forms.Button();
+			this.txt_supp_total = new System.Windows.Forms.TextBox();
+			this.lbl_supp_total = new System.Windows.Forms.Label();
+			this.lbl_supplier = new System.Windows.Forms.Label();
+			this.cbx_supplier = new System.Windows.Forms.ComboBox();
+			this.supp_dataGrid = new System.Windows.Forms.DataGridView();
+			this.id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.barcode = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.ItemType = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.qty = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.price = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.minqty = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.maxqty = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.orderqty = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			((System.ComponentModel.ISupportInitialize)(this.supp_dataGrid)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// btn_recived
+			// 
+			this.btn_recived.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.btn_recived.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btn_recived.Location = new System.Drawing.Point(611, 290);
+			this.btn_recived.Name = "btn_recived";
+			this.btn_recived.Size = new System.Drawing.Size(154, 36);
+			this.btn_recived.TabIndex = 26;
+			this.btn_recived.Text = "Enter Received";
+			this.btn_recived.UseVisualStyleBackColor = true;
+			// 
+			// btn_update
+			// 
+			this.btn_update.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.btn_update.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btn_update.Location = new System.Drawing.Point(611, 117);
+			this.btn_update.Name = "btn_update";
+			this.btn_update.Size = new System.Drawing.Size(154, 36);
+			this.btn_update.TabIndex = 25;
+			this.btn_update.Text = "Update Qty";
+			this.btn_update.UseVisualStyleBackColor = true;
+			// 
+			// txt_update_qty
+			// 
+			this.txt_update_qty.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.txt_update_qty.Location = new System.Drawing.Point(610, 85);
+			this.txt_update_qty.Name = "txt_update_qty";
+			this.txt_update_qty.Size = new System.Drawing.Size(155, 20);
+			this.txt_update_qty.TabIndex = 24;
+			// 
+			// lbl_qty
+			// 
+			this.lbl_qty.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.lbl_qty.AutoSize = true;
+			this.lbl_qty.Location = new System.Drawing.Point(606, 62);
+			this.lbl_qty.Name = "lbl_qty";
+			this.lbl_qty.Size = new System.Drawing.Size(75, 13);
+			this.lbl_qty.TabIndex = 23;
+			this.lbl_qty.Text = "Order Quantity";
+			// 
+			// lbl_order_date
+			// 
+			this.lbl_order_date.AutoSize = true;
+			this.lbl_order_date.Location = new System.Drawing.Point(3, 10);
+			this.lbl_order_date.Name = "lbl_order_date";
+			this.lbl_order_date.Size = new System.Drawing.Size(59, 13);
+			this.lbl_order_date.TabIndex = 22;
+			this.lbl_order_date.Text = "Order Date";
+			// 
+			// dateTimePicker1
+			// 
+			this.dateTimePicker1.Location = new System.Drawing.Point(68, 8);
+			this.dateTimePicker1.Name = "dateTimePicker1";
+			this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+			this.dateTimePicker1.TabIndex = 21;
+			// 
+			// btn_print_supp
+			// 
+			this.btn_print_supp.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.btn_print_supp.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btn_print_supp.Location = new System.Drawing.Point(435, 290);
+			this.btn_print_supp.Name = "btn_print_supp";
+			this.btn_print_supp.Size = new System.Drawing.Size(154, 36);
+			this.btn_print_supp.TabIndex = 20;
+			this.btn_print_supp.Text = "Print Form";
+			this.btn_print_supp.UseVisualStyleBackColor = true;
+			// 
+			// txt_supp_total
+			// 
+			this.txt_supp_total.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.txt_supp_total.Location = new System.Drawing.Point(612, 248);
+			this.txt_supp_total.Name = "txt_supp_total";
+			this.txt_supp_total.Size = new System.Drawing.Size(153, 20);
+			this.txt_supp_total.TabIndex = 19;
+			// 
+			// lbl_supp_total
+			// 
+			this.lbl_supp_total.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.lbl_supp_total.AutoSize = true;
+			this.lbl_supp_total.Location = new System.Drawing.Point(608, 225);
+			this.lbl_supp_total.Name = "lbl_supp_total";
+			this.lbl_supp_total.Size = new System.Drawing.Size(31, 13);
+			this.lbl_supp_total.TabIndex = 18;
+			this.lbl_supp_total.Text = "Total";
+			// 
+			// lbl_supplier
+			// 
+			this.lbl_supplier.AutoSize = true;
+			this.lbl_supplier.Location = new System.Drawing.Point(301, 10);
+			this.lbl_supplier.Name = "lbl_supplier";
+			this.lbl_supplier.Size = new System.Drawing.Size(45, 13);
+			this.lbl_supplier.TabIndex = 17;
+			this.lbl_supplier.Text = "Supplier";
+			// 
+			// cbx_supplier
+			// 
+			this.cbx_supplier.FormattingEnabled = true;
+			this.cbx_supplier.Location = new System.Drawing.Point(352, 7);
+			this.cbx_supplier.Name = "cbx_supplier";
+			this.cbx_supplier.Size = new System.Drawing.Size(248, 21);
+			this.cbx_supplier.TabIndex = 16;
+			// 
+			// supp_dataGrid
+			// 
+			this.supp_dataGrid.AllowUserToAddRows = false;
+			this.supp_dataGrid.AllowUserToDeleteRows = false;
+			this.supp_dataGrid.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.supp_dataGrid.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+			this.supp_dataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.supp_dataGrid.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.id,
+            this.barcode,
+            this.name,
+            this.ItemType,
+            this.qty,
+            this.price,
+            this.minqty,
+            this.maxqty,
+            this.orderqty});
+			this.supp_dataGrid.Location = new System.Drawing.Point(6, 62);
+			this.supp_dataGrid.Name = "supp_dataGrid";
+			this.supp_dataGrid.ReadOnly = true;
+			this.supp_dataGrid.RowHeadersVisible = false;
+			this.supp_dataGrid.RowHeadersWidth = 62;
+			this.supp_dataGrid.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+			this.supp_dataGrid.Size = new System.Drawing.Size(594, 206);
+			this.supp_dataGrid.TabIndex = 15;
+			// 
+			// id
+			// 
+			this.id.DataPropertyName = "Id";
+			this.id.HeaderText = "Id";
+			this.id.MinimumWidth = 8;
+			this.id.Name = "id";
+			this.id.ReadOnly = true;
+			this.id.Visible = false;
+			// 
+			// barcode
+			// 
+			this.barcode.DataPropertyName = "Barcode";
+			this.barcode.HeaderText = "Barcode";
+			this.barcode.MinimumWidth = 8;
+			this.barcode.Name = "barcode";
+			this.barcode.ReadOnly = true;
+			// 
+			// name
+			// 
+			this.name.DataPropertyName = "Name";
+			this.name.HeaderText = "Name";
+			this.name.MinimumWidth = 8;
+			this.name.Name = "name";
+			this.name.ReadOnly = true;
+			// 
+			// ItemType
+			// 
+			this.ItemType.DataPropertyName = "ItemType";
+			this.ItemType.HeaderText = "Type";
+			this.ItemType.MinimumWidth = 8;
+			this.ItemType.Name = "ItemType";
+			this.ItemType.ReadOnly = true;
+			this.ItemType.Visible = false;
+			// 
+			// qty
+			// 
+			this.qty.DataPropertyName = "Qty";
+			this.qty.HeaderText = "Qty";
+			this.qty.MinimumWidth = 8;
+			this.qty.Name = "qty";
+			this.qty.ReadOnly = true;
+			// 
+			// price
+			// 
+			this.price.DataPropertyName = "Price";
+			this.price.HeaderText = "Price";
+			this.price.MinimumWidth = 8;
+			this.price.Name = "price";
+			this.price.ReadOnly = true;
+			// 
+			// minqty
+			// 
+			this.minqty.DataPropertyName = "minqty";
+			this.minqty.HeaderText = "Min Qty";
+			this.minqty.MinimumWidth = 8;
+			this.minqty.Name = "minqty";
+			this.minqty.ReadOnly = true;
+			// 
+			// maxqty
+			// 
+			this.maxqty.DataPropertyName = "maxqty";
+			this.maxqty.HeaderText = "Max Qty";
+			this.maxqty.MinimumWidth = 8;
+			this.maxqty.Name = "maxqty";
+			this.maxqty.ReadOnly = true;
+			// 
+			// orderqty
+			// 
+			this.orderqty.DataPropertyName = "orderqty";
+			this.orderqty.HeaderText = "Order Qty";
+			this.orderqty.MinimumWidth = 8;
+			this.orderqty.Name = "orderqty";
+			this.orderqty.ReadOnly = true;
+			// 
+			// Panel_SupplierOrder
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.Controls.Add(this.btn_recived);
+			this.Controls.Add(this.btn_update);
+			this.Controls.Add(this.txt_update_qty);
+			this.Controls.Add(this.lbl_qty);
+			this.Controls.Add(this.lbl_order_date);
+			this.Controls.Add(this.dateTimePicker1);
+			this.Controls.Add(this.btn_print_supp);
+			this.Controls.Add(this.txt_supp_total);
+			this.Controls.Add(this.lbl_supp_total);
+			this.Controls.Add(this.lbl_supplier);
+			this.Controls.Add(this.cbx_supplier);
+			this.Controls.Add(this.supp_dataGrid);
+			this.Name = "Panel_SupplierOrder";
+			this.Size = new System.Drawing.Size(768, 329);
+			((System.ComponentModel.ISupportInitialize)(this.supp_dataGrid)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.Button btn_recived;
+		private System.Windows.Forms.Button btn_update;
+		private System.Windows.Forms.TextBox txt_update_qty;
+		private System.Windows.Forms.Label lbl_qty;
+		private System.Windows.Forms.Label lbl_order_date;
+		private System.Windows.Forms.DateTimePicker dateTimePicker1;
+		private System.Windows.Forms.Button btn_print_supp;
+		private System.Windows.Forms.TextBox txt_supp_total;
+		private System.Windows.Forms.Label lbl_supp_total;
+		private System.Windows.Forms.Label lbl_supplier;
+		private System.Windows.Forms.ComboBox cbx_supplier;
+		private System.Windows.Forms.DataGridView supp_dataGrid;
+		private System.Windows.Forms.DataGridViewTextBoxColumn id;
+		private System.Windows.Forms.DataGridViewTextBoxColumn barcode;
+		private System.Windows.Forms.DataGridViewTextBoxColumn name;
+		private System.Windows.Forms.DataGridViewTextBoxColumn ItemType;
+		private System.Windows.Forms.DataGridViewTextBoxColumn qty;
+		private System.Windows.Forms.DataGridViewTextBoxColumn price;
+		private System.Windows.Forms.DataGridViewTextBoxColumn minqty;
+		private System.Windows.Forms.DataGridViewTextBoxColumn maxqty;
+		private System.Windows.Forms.DataGridViewTextBoxColumn orderqty;
 	}
 }
---
+++ FrontEnd/frmOrdersPanels.Designer.cs
@@ -0,0 +1,181 @@
+﻿
+namespace SecretCellar {
+	partial class frmOrdersPanels {
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing) {
+			if (disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent() {
+			this.tabControl1 = new System.Windows.Forms.TabControl();
+			this.tab_CustomerRequest = new System.Windows.Forms.TabPage();
+			this.panel_CustomerRequest1 = new SecretCellar.Orders_Panels.Panel_CustomerRequest();
+			this.tab_SupplierOrder = new System.Windows.Forms.TabPage();
+			this.panel_SupplierOrder1 = new SecretCellar.Orders_Panels.Panel_SupplierOrder();
+			this.tab_CustomerOrder = new System.Windows.Forms.TabPage();
+			this.panel_CustomerOrder1 = new SecretCellar.Orders_Panels.Panel_CustomerOrder();
+			this.tab_Fulfillment = new System.Windows.Forms.TabPage();
+			this.panel_Fulfillment1 = new SecretCellar.Orders_Panels.Panel_Fulfillment();
+			this.tab_History = new System.Windows.Forms.TabPage();
+			this.panel_History1 = new SecretCellar.Orders_Panels.Panel_History();
+			this.tabControl1.SuspendLayout();
+			this.tab_CustomerRequest.SuspendLayout();
+			this.tab_SupplierOrder.SuspendLayout();
+			this.tab_CustomerOrder.SuspendLayout();
+			this.tab_Fulfillment.SuspendLayout();
+			this.tab_History.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// tabControl1
+			// 
+			this.tabControl1.Controls.Add(this.tab_CustomerRequest);
+			this.tabControl1.Controls.Add(this.tab_SupplierOrder);
+			this.tabControl1.Controls.Add(this.tab_CustomerOrder);
+			this.tabControl1.Controls.Add(this.tab_Fulfillment);
+			this.tabControl1.Controls.Add(this.tab_History);
+			this.tabControl1.Location = new System.Drawing.Point(12, 12);
+			this.tabControl1.Name = "tabControl1";
+			this.tabControl1.SelectedIndex = 0;
+			this.tabControl1.Size = new System.Drawing.Size(846, 356);
+			this.tabControl1.TabIndex = 0;
+			// 
+			// tab_CustomerRequest
+			// 
+			this.tab_CustomerRequest.Controls.Add(this.panel_CustomerRequest1);
+			this.tab_CustomerRequest.Location = new System.Drawing.Point(4, 22);
+			this.tab_CustomerRequest.Name = "tab_CustomerRequest";
+			this.tab_CustomerRequest.Padding = new System.Windows.Forms.Padding(3);
+			this.tab_CustomerRequest.Size = new System.Drawing.Size(838, 330);
+			this.tab_CustomerRequest.TabIndex = 0;
+			this.tab_CustomerRequest.Text = "Customer Request";
+			this.tab_CustomerRequest.UseVisualStyleBackColor = true;
+			// 
+			// panel_CustomerRequest1
+			// 
+			this.panel_CustomerRequest1.Location = new System.Drawing.Point(0, 0);
+			this.panel_CustomerRequest1.Name = "panel_CustomerRequest1";
+			this.panel_CustomerRequest1.Size = new System.Drawing.Size(836, 329);
+			this.panel_CustomerRequest1.TabIndex = 0;
+			// 
+			// tab_SupplierOrder
+			// 
+			this.tab_SupplierOrder.Controls.Add(this.panel_SupplierOrder1);
+			this.tab_SupplierOrder.Location = new System.Drawing.Point(4, 22);
+			this.tab_SupplierOrder.Name = "tab_SupplierOrder";
+			this.tab_SupplierOrder.Padding = new System.Windows.Forms.Padding(3);
+			this.tab_SupplierOrder.Size = new System.Drawing.Size(838, 330);
+			this.tab_SupplierOrder.TabIndex = 1;
+			this.tab_SupplierOrder.Text = "Supplier Order";
+			this.tab_SupplierOrder.UseVisualStyleBackColor = true;
+			// 
+			// panel_SupplierOrder1
+			// 
+			this.panel_SupplierOrder1.Location = new System.Drawing.Point(0, 0);
+			this.panel_SupplierOrder1.Name = "panel_SupplierOrder1";
+			this.panel_SupplierOrder1.Size = new System.Drawing.Size(838, 332);
+			this.panel_SupplierOrder1.TabIndex = 0;
+			// 
+			// tab_CustomerOrder
+			// 
+			this.tab_CustomerOrder.Controls.Add(this.panel_CustomerOrder1);
+			this.tab_CustomerOrder.Location = new System.Drawing.Point(4, 22);
+			this.tab_CustomerOrder.Name = "tab_CustomerOrder";
+			this.tab_CustomerOrder.Padding = new System.Windows.Forms.Padding(3);
+			this.tab_CustomerOrder.Size = new System.Drawing.Size(838, 330);
+			this.tab_CustomerOrder.TabIndex = 2;
+			this.tab_CustomerOrder.Text = "Customer Order";
+			this.tab_CustomerOrder.UseVisualStyleBackColor = true;
+			// 
+			// panel_CustomerOrder1
+			// 
+			this.panel_CustomerOrder1.Location = new System.Drawing.Point(0, 1);
+			this.panel_CustomerOrder1.Name = "panel_CustomerOrder1";
+			this.panel_CustomerOrder1.Size = new System.Drawing.Size(838, 329);
+			this.panel_CustomerOrder1.TabIndex = 0;
+			// 
+			// tab_Fulfillment
+			// 
+			this.tab_Fulfillment.Controls.Add(this.panel_Fulfillment1);
+			this.tab_Fulfillment.Location = new System.Drawing.Point(4, 22);
+			this.tab_Fulfillment.Name = "tab_Fulfillment";
+			this.tab_Fulfillment.Padding = new System.Windows.Forms.Padding(3);
+			this.tab_Fulfillment.Size = new System.Drawing.Size(838, 330);
+			this.tab_Fulfillment.TabIndex = 3;
+			this.tab_Fulfillment.Text = "Fulfillment";
+			this.tab_Fulfillment.UseVisualStyleBackColor = true;
+			// 
+			// panel_Fulfillment1
+			// 
+			this.panel_Fulfillment1.Location = new System.Drawing.Point(0, 1);
+			this.panel_Fulfillment1.Name = "panel_Fulfillment1";
+			this.panel_Fulfillment1.Size = new System.Drawing.Size(838, 329);
+			this.panel_Fulfillment1.TabIndex = 0;
+			// 
+			// tab_History
+			// 
+			this.tab_History.Controls.Add(this.panel_History1);
+			this.tab_History.Location = new System.Drawing.Point(4, 22);
+			this.tab_History.Name = "tab_History";
+			this.tab_History.Padding = new System.Windows.Forms.Padding(3);
+			this.tab_History.Size = new System.Drawing.Size(838, 330);
+			this.tab_History.TabIndex = 4;
+			this.tab_History.Text = "History";
+			this.tab_History.UseVisualStyleBackColor = true;
+			// 
+			// panel_History1
+			// 
+			this.panel_History1.Location = new System.Drawing.Point(-1, 1);
+			this.panel_History1.Name = "panel_History1";
+			this.panel_History1.Size = new System.Drawing.Size(836, 329);
+			this.panel_History1.TabIndex = 0;
+			// 
+			// frmOrdersPanels
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(869, 378);
+			this.Controls.Add(this.tabControl1);
+			this.Name = "frmOrdersPanels";
+			this.Text = "frmOrdersPanels";
+			this.tabControl1.ResumeLayout(false);
+			this.tab_CustomerRequest.ResumeLayout(false);
+			this.tab_SupplierOrder.ResumeLayout(false);
+			this.tab_CustomerOrder.ResumeLayout(false);
+			this.tab_Fulfillment.ResumeLayout(false);
+			this.tab_History.ResumeLayout(false);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TabControl tabControl1;
+		private System.Windows.Forms.TabPage tab_CustomerRequest;
+		private System.Windows.Forms.TabPage tab_SupplierOrder;
+		private System.Windows.Forms.TabPage tab_CustomerOrder;
+		private System.Windows.Forms.TabPage tab_Fulfillment;
+		private System.Windows.Forms.TabPage tab_History;
+		private Orders_Panels.Panel_CustomerRequest panel_CustomerRequest1;
+		private Orders_Panels.Panel_SupplierOrder panel_SupplierOrder1;
+		private Orders_Panels.Panel_CustomerOrder panel_CustomerOrder1;
+		private Orders_Panels.Panel_Fulfillment panel_Fulfillment1;
+		private Orders_Panels.Panel_History panel_History1;
+	}
+}
