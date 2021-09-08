08a71f1970fdfc861f1c5caca92e3b4e0e8df507
Heem Othman <heem.othman@gmail.com>
2021-09-07 21:13:18 -0500

    Added Checks To Add Printer Form
    -Added checks to Add Printer so it doesn't crash.
    
    -Added checks to PanHardware so it doesn't crash.

--- FrontEnd/Settings Panels/frmAddPrinter.cs
+++ FrontEnd/Settings Panels/frmAddPrinter.cs
@@ -1,13 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using Shared;
+
+
 
 namespace SecretCellar.Settings_Panels
 {
@@ -15,6 +11,8 @@ namespace SecretCellar.Settings_Panels
     {
         public Printer newPrinter = null;
         
+
+
         public frmAddPrinter()
         {
             InitializeComponent();
@@ -22,6 +20,8 @@ namespace SecretCellar.Settings_Panels
             cbx_manufact.DataSource = DataAccess.instance.GetPrinterMake();
             
         }
+
+
         public frmAddPrinter(Printer printer)
         {
             InitializeComponent();
@@ -38,65 +38,75 @@ namespace SecretCellar.Settings_Panels
 
         }
 
+
         private void btn_add_print_Click(object sender, EventArgs e)
         {
-            newPrinter = new Printer
-            {
-                Make = cbx_manufact.Text,
-                Model = txt_print_model.Text
-            };
-            newPrinter.Codes.Add(new PrinterCode { Cutter = txt_new_cut_code.Text, Drawer = txt_drawer_code.Text });
-            newPrinter.ModelId=DataAccess.instance.UpdatePrinter(newPrinter);
+            if (txt_print_model.Text == "" || txt_drawer_code.Text == "" || txt_new_cut_code.Text == "") {
+                MessageBox.Show("Model, Drawer Code, and Cut Code need to be filled out.", "Error");
+			}
+            else {
+                newPrinter = new Printer
+                {
+                    Make = cbx_manufact.Text,
+                    Model = txt_print_model.Text
+                };
+                newPrinter.Codes.Add(new PrinterCode { Cutter = txt_new_cut_code.Text, Drawer = txt_drawer_code.Text });
+                newPrinter.ModelId=DataAccess.instance.UpdatePrinter(newPrinter);
 
-            //this.DialogResult = DialogResult.OK;
-            populate();
-            clear_print_codes();
+                //this.DialogResult = DialogResult.OK;
+                populate();
+                clear_print_codes();
+			}
         }
+
 
         private void btn_close_Click(object sender, EventArgs e)
         {
             this.DialogResult = DialogResult.OK;
         }
+        
 
         private void btn_add_codes_Click(object sender, EventArgs e)
         {
-
             if (newPrinter != null && newPrinter.ModelId > 0)
             {
-
-
                 newPrinter.Codes.Add(new PrinterCode { Cutter = txt_new_cut_code.Text, Drawer = txt_drawer_code.Text });
                 DataAccess.instance.UpdatePrinter(newPrinter);
                 clear_print_codes();
+
+                populate();
             }
             else
             {
                 MessageBox.Show("Must add new printer before adding codes");
             }
 
-            populate();
             //this.DialogResult = DialogResult.OK;
         }
+
+
         private void clear_print_codes()
         {
             txt_drawer_code.Text = "";
             txt_new_cut_code.Text = "";
         }
 
-        private void populate()
-        {
-            newPrinter = DataAccess.instance.GetPrinter(newPrinter.ModelId);
-            grid_print_codes.AutoGenerateColumns = false;
-            grid_print_codes.DataSource = newPrinter.Codes;
-        }
 
         private void btn_delete_codes_Click(object sender, EventArgs e)
         {
+            if (grid_print_codes.SelectedRows.Count > 0) {
+                PrinterCode pc = newPrinter.Codes.First(x => x.CodeId == (uint)grid_print_codes.SelectedRows[0].Cells["CodeId"].Value);
+                newPrinter.Codes.Remove(pc);
+                DataAccess.instance.UpdatePrinter(newPrinter);
+                populate();
+			}
+        }
 
-            PrinterCode pc = newPrinter.Codes.First(x => x.CodeId == (uint)grid_print_codes.SelectedRows[0].Cells["CodeId"].Value);
-            newPrinter.Codes.Remove(pc);
-            DataAccess.instance.UpdatePrinter(newPrinter);
-            populate();
+
+        private void populate() {
+            newPrinter = DataAccess.instance.GetPrinter(newPrinter.ModelId);
+            grid_print_codes.AutoGenerateColumns = false;
+            grid_print_codes.DataSource = newPrinter.Codes;
         }
     }
 }
