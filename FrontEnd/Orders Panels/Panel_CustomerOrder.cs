655912763451c5e9bb5a6dcc67078d16a8ca655e
ggrabau <37936446+ggrabau@users.noreply.github.com>
2021-09-14 19:31:04 -0500

    order forms
    removed ? causing issues

--- FrontEnd/Orders Panels/Panel_CustomerOrder.cs
+++ FrontEnd/Orders Panels/Panel_CustomerOrder.cs
@@ -66,7 +66,7 @@ namespace SecretCellar.Orders_Panels {
 		/// <param name="e"></param>
 		private void btnFavoritesRemove_Click(object sender, EventArgs e) {
 			uint cid = ((Customer)cbx_cust_custorder.SelectedItem).CustomerID;
-			uint iid = inventory.FirstOrDefault(x => x.Name == custOrder_datagrid.SelectedRows[0].Cells["Name"].Value.ToString()).Id;
+			uint iid = inventory.FirstOrDefault(x => x.Name == custOrder_datagrid.SelectedRows[0].Cells["CustOrdName"].Value.ToString()).Id;
 
 			DataAccess.instance.DeleteCustomerFavorite(cid, iid);
 
