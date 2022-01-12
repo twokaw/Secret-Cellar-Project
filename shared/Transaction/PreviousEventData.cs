2bc55077725553e71f8f5609652c33ab43237b3f
Bill Pitcher <wjpitcher@gmail.com>
2022-01-11 20:47:42 -0600

    Add properties to previous

--- shared/Transaction/PreviousEventData.cs
+++ shared/Transaction/PreviousEventData.cs
@@ -1,11 +1,11 @@
 ﻿namespace Shared {
 	public class PreviousEventData {
-		public double TotalSold;
-		public double PreOrderSold;
-		public double AtDoorSold;
+		public double TotalSold { get; set; }
+		public double PreOrderSold { get; set; }
+		public double AtDoorSold { get; set; }
 
-		public double TotalProfit;
-		public double PreOrderProfit;
-		public double AtDoorProfit;
+		public double TotalProfit { get; set; }
+		public double PreOrderProfit { get; set; }
+		public double AtDoorProfit { get; set; }
 	}
 }
