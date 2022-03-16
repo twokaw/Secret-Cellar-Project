2a56ef279391e91af619078d0fe7e442d939e78d
ggrabau <37936446+ggrabau@users.noreply.github.com>
2022-03-15 19:52:33 -0500

    Create EncryptionClass.cs

---
+++ backend/pos-core-api/Helpers/EncryptionClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+
+namespace pos_core_api.Helpers
+{
+    public class EncryptionClass
+    {
+        Aes aes = Aes.Create();
+    }
+}
