ce220120a13f424c43424c0506d419388074e7fb
Bill Pitcher <wjpitcher@gmail.com>
2022-05-05 21:49:00 -0500

    Invoices and panTypes
    create a new invoice when items are delivered

--- FrontEnd/Settings Panels/PanTypes.cs
+++ FrontEnd/Settings Panels/PanTypes.cs
@@ -25,8 +25,6 @@ namespace SecretCellar.Settings_Panels
                 cbx_tax.DataSource = DataAccess.instance?.GetTax();
                 lstTypes.DisplayMember = "TypeName";
                 cbx_tax.DisplayMember = "TaxName";
-            
-                
             }
         }
 
@@ -136,14 +134,13 @@ namespace SecretCellar.Settings_Panels
 
         private void panel3_VisibleChanged(object sender, EventArgs e)
         {
-            if (DataAccess.instance != null)
-                getDiscounts();
+            getDiscounts();
         }
 
         private void getDiscounts()
         {
             chk_lst_discount.Items.Clear();
-            discounts = DataAccess.instance.GetDiscount();
+            discounts = DataAccess.instance?.GetDiscount();
             discounts?.ForEach(x => chk_lst_discount.Items.Add(x.DiscountName));
         }
     }
