4b1682d3c4f563353a74de98727f1d2ba18ad953
ggrabau <37936446+ggrabau@users.noreply.github.com>
2022-05-07 23:13:48 -0500

    employee settings update

--- backend/pos-core-api/ORM/EmployeeTypeORM.cs
+++ backend/pos-core-api/ORM/EmployeeTypeORM.cs
@@ -135,7 +135,27 @@ namespace pos_core_api.ORM
                 db.CloseCommand(cmd);
             }
         }
+        public List<EmployeeRoleModel> GetRoles()
+        {
+            List<EmployeeRoleModel> empRoles = new List<EmployeeRoleModel>();
+            MySqlCommand cmd = db.CreateCommand(@"
+                select * from employee_role            
+            ");
+            MySqlDataReader reader = cmd.ExecuteReader();
+            try
+            {
+                while (reader.Read())
+                    empRoles.Add(new EmployeeRoleModel() 
+                    {
+                        RoleID = reader.IsDBNull("roleID")? 0 : reader.GetUInt32("roleID"),
+                        RoleName = reader.IsDBNull("roleName")?"" : reader.GetString("roleName"),
+                        RoleDescription = reader.IsDBNull("roleDescription")? "" : reader.GetString("roleDescription")         
+                    });
 
+            }
+            finally { reader.Close(); }
+            return empRoles;
+        }
         public void UpdateTypeRoles(EmployeeTypeModel empType)
         {
 
