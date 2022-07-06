d63f6e2e3a7296d1c4d5bdfd8abc00f923a2373a
Heem Othman <heem.othman@gmail.com>
2022-07-05 20:07:11 -0500

    Update Invoice.cs
    -Updated print preview page.

--- hardware/NCR_Printer/Invoice.cs
+++ hardware/NCR_Printer/Invoice.cs
@@ -19,64 +19,54 @@ namespace NCR_Printer {
         public override void PrintPage(object sender, PrintPageEventArgs e) {
             SetupPage(e);
             float y = cursor.Y;
-            Font potitle = new Font("Arial", 12, FontStyle.Bold);
+            Font titleFont = new Font("Arial", 12, FontStyle.Bold);
+            
+            //HEADER
             PrintImage(Layout.Logo, (float)0.4, TextAlignment.Left);
             this.cursor.Y = (cursor.Y - y) / 2;
-            PrintText($"Purchase Order", potitle, true, TextAlignment.Center);
+            PrintText($"Invoice", titleFont, true, TextAlignment.Center);
             PrintText("", true);
-            // Print Header
             PrintHeaderFooter(Layout.Header);
 
-            string bcode = "Barcode";
-            string name = "Name";
-            string price = "Price";
-            string oqty = "Order Qty";
-            string qtyrec = "Received Qty";
-
             PrintText("", true);
             PrintText("", true);
 
-            PrintText($"ID: {_invoice.InvoiceID}", true, TextAlignment.Center);
+            //INVOICE ID AND CUSTOMER NAME
+            PrintText($"ID: {_invoice.InvoiceID}", 0);
+            PrintText($"{_invoice.CustomerName}", 80);
 
-            PrintText($"{_invoice.CustomerName}", true, TextAlignment.Center);
-            //if (!string.IsNullOrWhiteSpace(_invoice.))
-                //PrintText($"{supplier.Phone}", true, TextAlignment.Center);
-
-
-
-            // Date and time
-
-            PrintText($"Date: {DateTime.Now:M/dd/yy}", true, TextAlignment.Right);
-
+            //DATE AND TIME
+            cursor.X = 0;
+            PrintText($"{DateTime.Now:M/dd/yy}", true, TextAlignment.Right);
             PrintHorizontalLine();
 
-            //PrintText($"{bcode,6} {name,35} {price,40}{oqty,25}{qtyrec,40}");
+            //TABLE HEADERS
+            PrintText("Barcode", titleFont, 5);
+            PrintText("Item", titleFont, 200);
+            PrintText("Price", titleFont, 550);
+            PrintText("Number Sold", titleFont, 630);
+            cursor.X = 0;
+            PrintText("Total", titleFont, true, TextAlignment.Right);
 
-            PrintText(bcode, 5);
-            PrintText(name, 200);
-            PrintText(price, 550);
-            PrintText(oqty, 650);
-            PrintText(qtyrec, 750, true);
-            //PrintText($"\t\t\t\t\t Barcode  \t\t\t\t\t Name  \t\t\t\t\t Price  \t\t\t\t\t Order Qty  \t\t\t\t\t Qty Received",true);
             double total = 0;
             cursor.X = 0;
             PrintHorizontalLine();
 
-            //foreach (Inventory item in inventory) {
-            //    PrintText(item.Barcode, 5);
-            //    PrintText(item.Name, 200);
-            //    PrintText(item.Price.ToString(), 550);
-            //    PrintText(item.OrderQty.ToString(), 680, true);
-            //    PrintText($"*{item.Barcode}*", Layout.BarcodeFont, 5, true);
-            //    total += item.OrderQty * item.Price;
-            //    //PrintText($"{item.Price:C}", true, TextAlignment.Right);
-            //}
+            //TABLE
+            foreach (Item item in _invoice.Items) {
+                PrintText(item.Barcode, 5);
+                PrintText(item.Name, 200);
+                PrintText(item.Price.ToString(), 550);
+                PrintText(item.NumSold.ToString(), 630);
+                cursor.X = 0;
+                PrintText((item.Price * item.NumSold).ToString(), true, TextAlignment.Right);
+                total += item.Price * item.NumSold;
+            }
 
             cursor.X = 0;
             PrintHorizontalLine();
             cursor.X = 0;
-            PrintText($"Total: {total:C}", true, TextAlignment.Right);
-
+            PrintText($"{total:C}", true, TextAlignment.Right);
         }
     }
 }
