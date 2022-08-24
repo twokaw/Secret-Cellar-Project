69bff8b66e54db473d93f7b92dc2f49daf655ea9
Heem Othman <heem.othman@gmail.com>
2022-08-23 19:40:59 -0500

    Added start to manager override

---
+++ FrontEnd/frmManagerOverride.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace SecretCellar {
+    partial class frmManagerOverride {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.label_ManagerPassword = new System.Windows.Forms.Label();
+            this.textBox_ManagerPassword = new System.Windows.Forms.TextBox();
+            this.button_Cancel = new System.Windows.Forms.Button();
+            this.button_Confirm = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label_ManagerPassword
+            // 
+            this.label_ManagerPassword.AutoSize = true;
+            this.label_ManagerPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_ManagerPassword.Location = new System.Drawing.Point(12, 30);
+            this.label_ManagerPassword.Name = "label_ManagerPassword";
+            this.label_ManagerPassword.Size = new System.Drawing.Size(130, 25);
+            this.label_ManagerPassword.TabIndex = 0;
+            this.label_ManagerPassword.Text = "Manager Id";
+            // 
+            // textBox_ManagerPassword
+            // 
+            this.textBox_ManagerPassword.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox_ManagerPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_ManagerPassword.Location = new System.Drawing.Point(17, 58);
+            this.textBox_ManagerPassword.Name = "textBox_ManagerPassword";
+            this.textBox_ManagerPassword.PasswordChar = '*';
+            this.textBox_ManagerPassword.Size = new System.Drawing.Size(369, 31);
+            this.textBox_ManagerPassword.TabIndex = 1;
+            // 
+            // button_Cancel
+            // 
+            this.button_Cancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button_Cancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button_Cancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_Cancel.Location = new System.Drawing.Point(17, 107);
+            this.button_Cancel.Name = "button_Cancel";
+            this.button_Cancel.Size = new System.Drawing.Size(87, 44);
+            this.button_Cancel.TabIndex = 2;
+            this.button_Cancel.Text = "Cancel";
+            this.button_Cancel.UseVisualStyleBackColor = true;
+            this.button_Cancel.Click += new System.EventHandler(this.button_Cancel_Click);
+            // 
+            // button_Confirm
+            // 
+            this.button_Confirm.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button_Confirm.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.button_Confirm.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_Confirm.Location = new System.Drawing.Point(299, 107);
+            this.button_Confirm.Name = "button_Confirm";
+            this.button_Confirm.Size = new System.Drawing.Size(87, 44);
+            this.button_Confirm.TabIndex = 3;
+            this.button_Confirm.Text = "Confirm";
+            this.button_Confirm.UseVisualStyleBackColor = true;
+            this.button_Confirm.Click += new System.EventHandler(this.button_Confirm_Click);
+            // 
+            // frmManagerOverride
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(247)))), ((int)(((byte)(231)))), ((int)(((byte)(206)))));
+            this.ClientSize = new System.Drawing.Size(398, 163);
+            this.Controls.Add(this.button_Confirm);
+            this.Controls.Add(this.button_Cancel);
+            this.Controls.Add(this.textBox_ManagerPassword);
+            this.Controls.Add(this.label_ManagerPassword);
+            this.Name = "frmManagerOverride";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Manager Override";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label_ManagerPassword;
+        private System.Windows.Forms.TextBox textBox_ManagerPassword;
+        private System.Windows.Forms.Button button_Cancel;
+        private System.Windows.Forms.Button button_Confirm;
+    }
+}
---
+++ FrontEnd/frmManagerOverride.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+
+
+namespace SecretCellar {
+    public partial class frmManagerOverride : Form {
+        public frmManagerOverride() { InitializeComponent(); }
+        
+
+        /// <summary>
+        /// Cancels the manager override.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button_Cancel_Click(object sender, EventArgs e) { Close(); }
+
+
+        /// <summary>
+        /// Checks that the entered manager id is correct and closes the form with a dialogResult of OK.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button_Confirm_Click(object sender, EventArgs e) {
+            Console.WriteLine(label_ManagerPassword.Text);
+
+
+
+        }
+    }
+}
