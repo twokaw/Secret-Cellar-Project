1df1ebf4c3ae361089d942445f86cb77ab7a4792
Heem Othman <heem.othman@gmail.com>
2023-09-07 19:37:40 -0500

    Update frmReceivedOrders.cs
    -Fixed bug with receiving more than was ordered for an item. Made it so a manager can override it.
    
    -Added sanity checks to the receive all method.

--- FrontEnd/frmReceivedOrders.cs
+++ FrontEnd/frmReceivedOrders.cs
@@ -53,55 +53,81 @@ namespace SecretCellar
             refresh();
         }
 
-        private void btn_update_Click(object sender, EventArgs e)
-        {
+        private void btn_update_Click(object sender, EventArgs e) {
+            if (received_dataGrid.SelectedRows.Count <= 0) {
+				txt_received_qty.Focus();
+				txt_received_qty.SelectAll();
+				MessageBox.Show("Quantity received needs to be greater than 0.");
+				return;
+			}
 
-            if (received_dataGrid.SelectedRows.Count > 0)
-            {
-                
-                Inventory i = DataAccess.instance.GetInventory().First(x => x.Id == uint.Parse(received_dataGrid.SelectedRows[0].Cells["id"].Value.ToString()));
-                
+			Inventory inventory = DataAccess.instance.GetInventory().First(x => x.Id == uint.Parse(received_dataGrid.SelectedRows[0].Cells["id"].Value.ToString()));
+            if (inventory == null) {
+				txt_received_qty.Focus();
+				txt_received_qty.SelectAll();
+				MessageBox.Show("Inventory item was not found.");
+				return;
+			}
 
-                if (uint.TryParse(txt_received_qty.Text.Trim(), out uint order)) 
-                {
+			if (uint.TryParse(txt_received_qty.Text.Trim(), out uint quantityToReceive)) {
+                if (quantityToReceive > inventory.OrderQty) {
+                    if (!frmManagerOverride.DidOverride("Quantity to receive is greater than quantity ordered.")) {
+						txt_received_qty.Focus();
+						txt_received_qty.SelectAll();
+						return;
+					}
+				}
 
-                    if (i.OrderQty >= order)
-                    {
-                        i.OrderQty -= order;
-                    }
-                    else
-                    {
-                        i.OrderQty = 0;
-                    }
-                    
-                   
-                    if (i.AllQty.Count == 0)
-                    {
-                        i.AllQty.Add(new InventoryQty { Qty = order, PurchasedDate = DateTime.Now, SupplierPrice = 0 });
-                    }
-                    else
-                    {
-                        i.AllQty[0].Qty += order;
-                        i.AllQty[0].PurchasedDate = DateTime.Now;
-                    }
-                }
-                else
-                {
-                    txt_received_qty.Focus();
-                    txt_received_qty.SelectAll();
-                    MessageBox.Show("Invalid Quantity");
+				if (inventory.OrderQty >= quantityToReceive) inventory.OrderQty -= quantityToReceive;
+				else inventory.OrderQty = 0;
 
-                    return;
-                }
+				if (inventory.AllQty.Count == 0) {
+					inventory.AllQty.Add(new InventoryQty { Qty = quantityToReceive, PurchasedDate = DateTime.Now, SupplierPrice = 0 });
+				}
+				else {
+					inventory.AllQty[0].Qty += quantityToReceive;
+					inventory.AllQty[0].PurchasedDate = DateTime.Now;
+				}
+			}
+			else {
+				txt_received_qty.Focus();
+				txt_received_qty.SelectAll();
+				MessageBox.Show("Invalid Quantity");
+				return;
+			}
 
-                DataAccess.instance.UpdateItem(i);
-            }
+			DataAccess.instance.UpdateItem(inventory);
 
-            refresh();
+			refresh();
             txt_received_qty.Text = "";
         }
 
-        private void refresh()
+
+		private void btn_all_received_Click(object sender, EventArgs e) {
+			foreach (DataGridViewRow row in received_dataGrid.Rows) {
+				Inventory inventory = DataAccess.instance.GetInventory().First(x => x.Id == uint.Parse(row.Cells["id"].Value.ToString()));
+				if (inventory == null) {
+					MessageBox.Show("Inventory item was not found.");
+					continue;
+				}
+
+				if (inventory.AllQty.Count == 0) {
+					inventory.AllQty.Add(new InventoryQty { Qty = inventory.OrderQty, PurchasedDate = DateTime.Now, SupplierPrice = 0 });
+				}
+				else {
+					inventory.AllQty[0].Qty += inventory.OrderQty;
+					inventory.AllQty[0].PurchasedDate = DateTime.Now;
+				}
+
+				inventory.OrderQty = 0;
+				DataAccess.instance.UpdateItem(inventory);
+			}
+
+			refresh();
+		}
+
+
+		private void refresh()
         {
             uint id = ((Supplier)cbx_supplier.SelectedItem).SupplierID;
             received_dataGrid.DataSource = DataAccess.instance.GetInventory().Where(x => (id == x.SupplierID || id == 0) && x.OrderQty > 0).
@@ -120,28 +146,10 @@ namespace SecretCellar
                ToList();
         }
 
-        private void btn_close_Click(object sender, EventArgs e)
-        {
-            this.Close();
-        }
 
-        private void btn_all_received_Click(object sender, EventArgs e)
-        {
-            foreach (DataGridViewRow row in received_dataGrid.Rows)
-            {
-                Inventory i = DataAccess.instance.GetInventory().First(x => x.Id == uint.Parse(row.Cells["id"].Value.ToString()));
-                i.AllQty[0].Qty += uint.Parse(row.Cells["OrderQty"].Value.ToString());
-                i.OrderQty = 0;
-                i.AllQty[0].PurchasedDate = DateTime.Now;
-                DataAccess.instance.UpdateItem(i);
-            }
+        private void btn_close_Click(object sender, EventArgs e) { this.Close(); }
 
-            refresh();
-        }
-
-        private void frmReceivedOrders_Load(object sender, EventArgs e)
-        {
-
-        }
+        
+        private void frmReceivedOrders_Load(object sender, EventArgs e) { }
     }
 }
